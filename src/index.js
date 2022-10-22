54118b3c2dfeab13a200e4831d37ece032e10bfa
Callum McLuskey <callummcluskey100@gmail.com>
2022-10-22 15:20:09 +0100

    removed duplicate code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import {DeviceController} from '@espruino-tools/device-controller'
 import { showTemplate } from './espruino-template';
 import './styles/app.scss'
-const device = new DeviceController();
-
-export const connect = () => device.connect()
 
 window.onload = function(){
    showTemplate()
