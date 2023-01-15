6d94353ccd56d2374db4da36c953c79ff926ec46
Callum McLuskey <callummcluskey100@gmail.com>
2023-01-15 14:21:20 +0000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ npm run build
 
 git checkout -b gh-pages
 
+mv public/images ./build
+
 find . -maxdepth 1 ! -name '.git' ! -name 'build' ! -name '.gitignore' ! -name 'node_modules' -exec rm -rf {} \;
 
 mv build/* .
@@ -17,5 +19,4 @@ git commit -m "Deploy to Github Pages"
 git push origin gh-pages
 
 git checkout -
-
 git branch -D gh-pages
